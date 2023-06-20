38fbe3d5a36b8b945835dbdec0586171af26d5d1
i-m-rishabh <uniquepandey125@gmail.com>
2023-06-20 23:57:34 +0530

    added loading logo animatation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import FoodList from "./components/Meals/FoodList";
 import Summary from "./components/Summary";
 import './App.css';
 import ItemCart from "./components/Cart/ItemCart";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartProvider from './components/store/CartProvider'
+import Loading from "./components/UI/Loading";
+
 const App = () => {
   const [isCartOpen, setCartOpen] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setLoading(false);
+    },3000)
+    return ()=>{ clearTimeout(timer)}
+  },[])
+
   function handleCardClick(){
     setCartOpen(true);
   }
@@ -15,6 +25,8 @@ const App = () => {
   }
   return(
     <CartProvider >
+    { isLoading ? <Loading/> 
+      :
       <div className='app'>
         {console.log('app re evaluated')}
         <Header onCartOpen={handleCardClick} />
@@ -22,6 +34,7 @@ const App = () => {
         <FoodList />
         {isCartOpen && <ItemCart onCartClose={handleCartClose} />}
       </div>
+    }
     </CartProvider>
   )
 }
---
+++ src/components/UI/Loading.js
@@ -0,0 +1,14 @@
+import classes from './loading.module.css';
+
+const Loading = () => {
+    
+    return(
+        <div className={classes.container}>
+            <div className={classes.box}>
+                <h1>REACT MEAL</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Loading;
